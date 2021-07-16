ab47eb4f62e7e13ebf9d4569d4d582f3c0056953
Jenna Toff <shamanlvr@gmail.com>
2021-07-16 10:35:12 -0700

    added form validation

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav__list");
 const navLinks = document.querySelectorAll(".nav__link");
+const form = document.querySelector('.form')
+const emailInput = form.querySelector('.form__email-input');
+const message = form.querySelector('.form__textarea');
+const submitButton = form.querySelector('.form__btn_type_submit')
 
 // Function to toggle hamburger
 const toggleHamburger = () => {
@@ -18,3 +22,13 @@ const closeMobileMenu = () => {
 }
 
 navLinks.forEach(navLink => navLink.addEventListener("click", closeMobileMenu));
+
+// form validation
+function validate() {
+  submitButton.disabled = true;
+  if (emailInput.validity.valid && message.validity.valid) {
+    submitButton.disabled = false
+  }
+}
+emailInput.addEventListener('keypress', validate);
+message.addEventListener('keypress', validate);
